d68baf35fef3a93de1abed1606347fda614ff71d
Giuseppe <132507608+GiuseppeTaylorr@users.noreply.github.com>
2023-07-12 17:00:43 +0000

    subiendo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generar() {
+  //La forma para declarar un arreglo es la siguiente
+  let combinations = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let k = 0; k < adj.length; k++) {
+      for (let j = 0; j < noun.length; j++) {
+        let combinations = pronoun[i] + adj[k] + noun[j]+ ".net";
+        combinations.push(combinations);
+      }
+    }
+  }
+  return combinations;
+}
+let allCombinations = generar();
+console.log(allCombinations);
